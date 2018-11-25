3e3444731c6b9e6b7dbce1a022bb6a9a187a5238
Norrapat Nimmanee <norrapat@genesiscorp.tech>
2018-11-25 23:57:29 +0700

    add file name log

--- webapp.js
+++ webapp.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const multer = require('multer');
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, __dirname + '/uploads');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
 });
 const upload = multer({
-    storage: storage
+  storage,
 });
 const app = express();
 
 app.use('/', express.static(__dirname + '/static'));
 app.post('/upload', upload.single('upload'), (req, res, next) => {
-    res.sendFile(__dirname + '/static/success.html');
+  const {
+    filename,
+    originalname,
+  } = req.file;
+  console.log('File name :', filename);
+  console.log('Original File name :', originalname);
+
+  res.sendFile(__dirname + '/static/success.html');
 });
 
 app.listen(8000, () => {
-    console.log('Express server listening on port 8000');
+  console.log('Express server listening on port 8000');
 });
